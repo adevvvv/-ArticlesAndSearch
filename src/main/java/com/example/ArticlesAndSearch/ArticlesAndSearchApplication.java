cbedb89de38c421b5edd0a6cf18bcb11fea5a88c
adevvvv <adevvv@bk.ru>
2024-02-29 09:18:50 +0300

    feature: Создай стартовый проект и добавь в maven - Spring (Web, MongoDB)

---
+++ src/main/java/com/example/ArticlesAndSearch/ArticlesAndSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArticlesAndSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesAndSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesAndSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ArticlesAndSearch/ArticlesAndSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArticlesAndSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesAndSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
